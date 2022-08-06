d1852b3ebe36bfb7cfae60bd9105238bec99c4b8
BTC Oracle <carlhamilton1020@gmail.com>
2022-08-06 12:19:00 +1000

    Contract development update - added 2 lines

--- src/store/strategyRanges.js
+++ src/store/strategyRanges.js
@@ -1,1 +1,5 @@
 import { createSlice, current } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { roundToNearestTick } from "../helpers/uniswap/liquidity";
+import chartColors from "../data/colors.json";
+import { round } from "../helpers/numbers";
