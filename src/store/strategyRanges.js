62d8b7d5346ac0c0f9b4c89721c536cd8d8a13cc
BTC Oracle <carlhamilton1020@gmail.com>
2022-08-16 16:44:00 +1000

    Contract development update - added 4 lines

--- src/store/strategyRanges.js
+++ src/store/strategyRanges.js
@@ -3,3 +3,11 @@ import { createSelector } from "reselect";
 import { roundToNearestTick } from "../helpers/uniswap/liquidity";
 import chartColors from "../data/colors.json";
 import { round } from "../helpers/numbers";
+
+const initialState ={type: "amount", strategies: [
+  {id: "S1", name: "Strategy 1", color: chartColors['uniswap'].S1, 
+    inputs: { min: { value: 1, name: "Min", label: "Min Range S1", percent: 0 }, max: {value: 1, name: "Max", label: "Max Range S1", percent: 0 } },
+    liquidityMultiplier: 1, selected: true, leverage: 1, rangesEditable: true, tokenratio: {token0: 0.5, token1: 0.5}, hedging: {type: "short", leverage: 1, amount: 0}
+  }, 
+  {id: "S2", name: "Strategy 2", color: chartColors['uniswap'].S2, 
+    inputs:  { min: { value: 1, name: "Min", label: "Min Range S1", percent: 0 }, max: {value: 1, name: "Max", label: "Max Range S2", percent: 0 } },
