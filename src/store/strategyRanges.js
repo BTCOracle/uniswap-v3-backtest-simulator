59bdb9aa1584568b4c8dd27ef89759b2581dd43d
BTC Oracle <carlhamilton1020@gmail.com>
2022-09-04 10:05:00 +1000

    Contract development update - added 2 lines

--- src/store/strategyRanges.js
+++ src/store/strategyRanges.js
@@ -13,3 +13,13 @@ const initialState ={type: "amount", strategies: [
     inputs:  { min: { value: 1, name: "Min", label: "Min Range S1", percent: 0 }, max: {value: 1, name: "Max", label: "Max Range S2", percent: 0 } },
     liquidityMultiplier: 1, selected: false, leverage: 1, rangesEditable: true, tokenratio: {token0: 0.5, token1: 0.5}, hedging: {type: "short", leverage: 1, amount: 0}
   },
+  {id: "v2", name: "Unbounded", color: chartColors['uniswap'].v2, 
+    inputs:  { min: { value: Math.pow(1.0001, -887220), name: "Min", label: "Min Range V2", percent: 0 }, max: {value: Math.pow(1.0001, 887220), name: "Max", label: "Max Range V2", percent: 0 } },
+    liquidityMultiplier: 1, selected: true, leverage: 1, rangesEditable: false, tokenratio: {token0: 0.5, token1: 0.5}, hedging: {type: "short", leverage: 1, amount: 0}
+  }]} ;
+
+const calcContrentratedLiquidityMultiplier = (min, max) => {
+  return Math.round((1 / (1 - Math.pow((min / max), 0.25))) * 100) / 100;
+}
+
+export const validateStrategyRangeValue = (strategy, key, value) => {
